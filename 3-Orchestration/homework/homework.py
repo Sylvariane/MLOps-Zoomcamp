48fb624ac1410daaf9f88a9f3ca5932504181fa9
Sylvariane <cecile.guillot08@protonmail.com>
2022-06-12 22:35:50 +0200

    Update

--- 3-Orchestration/homework/homework.py
+++ 3-Orchestration/homework/homework.py
@@ -1,13 +1,26 @@
 import pandas as pd
+import os
 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
+from prefect import flow, task
+from prefect.task_runners import SequentialTaskRunner
+from prefect.utilities.logging import get_run_logger
+
+
+@task
+def get_paths():
+    os.system(f"wget {url} -O {parquet_name}")
+
+
+@task
 def read_data(path):
     df = pd.read_parquet(path)
     return df
 
+@task
 def prepare_features(df, categorical, train=True):
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
@@ -22,8 +35,9 @@ def prepare_features(df, categorical, train=True):
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
     return df
 
+@task
 def train_model(df, categorical):
-
+    logger = prefect.context.get("logger")
     train_dicts = df[categorical].to_dict(orient='records')
     dv = DictVectorizer()
     X_train = dv.fit_transform(train_dicts) 
@@ -39,6 +53,7 @@ def train_model(df, categorical):
     print(f"The MSE of training is: {mse}")
     return lr, dv
 
+@task
 def run_model(df, categorical, dv, lr):
     val_dicts = df[categorical].to_dict(orient='records')
     X_val = dv.transform(val_dicts) 
@@ -49,16 +64,18 @@ def run_model(df, categorical, dv, lr):
     print(f"The MSE of validation is: {mse}")
     return
 
-def main(train_path: str = './data/fhv_tripdata_2021-01.parquet', 
-           val_path: str = './data/fhv_tripdata_2021-02.parquet'):
+@flow(task_runner=SequentialTaskRunner())
+@flow
+def main(date=None):
+    train_path, val_path = get_paths(date).result()
 
     categorical = ['PUlocationID', 'DOlocationID']
 
     df_train = read_data(train_path)
-    df_train_processed = prepare_features(df_train, categorical)
+    df_train_processed = prepare_features(df_train, categorical).result()
 
     df_val = read_data(val_path)
-    df_val_processed = prepare_features(df_val, categorical, False)
+    df_val_processed = prepare_features(df_val, categorical, False).result()
 
     # train the model
     lr, dv = train_model(df_train_processed, categorical)
